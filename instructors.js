e043765579e4d5289122a1268ea9e04542a07aed
weebcodr <joeldavilaromero18@gmail.com>
2026-09-02 20:52:00 -0700

    Instructors update

--- instructors.js
+++ instructors.js
@@ -10,24 +10,16 @@ const instructors = [
     },
 
     {
-        name: "Daniel Durrand - Flight Instructor",
-        credentials: "CFI, CFII",
-        photo: "assets/images/Instructors/Daniel Durrand.jpg",
+        name: "Josh Friedman - Flight Instructor",
+        credentials: "CFI, CFII, ATP",
+        photo: "assets/images/Instructors/Josh Friedman2.jpg",
         specialties: ["Private", "Instrument", "Single Engine Commercial", "Flight Review", "Instrument Proficiency Check"],
         approvedAircraft: ["C172", "PA28"], 
-        bio: "Let's get you flying! When I started flight training, I was a weekend warrior with an insurance day job, but what started as a fun and challenging hobby became an exciting passion and rewarding career. Regardless of your goals, flying offers you the opportunity to tackle new challenges, embrace adventure, and (literally) see the world from a new perspective. Whether you're looking to get in the plane just for fun, or to eventually fly professionally, I will help you become a safe and capable pilot. I look forward to sharing the cockpit with you!",
+        bio: "Let's go flying! Since I was a kid I dreamed of becoming a pilot. Unfortunately at the time I didn't know any and it seemed impossible. All of that has changed! I'm now a part of the aviation community. So whether you're interested in a new hobby and sailing seems a bit too slow, or you're pursuing a career path such as myself, we'll work together to make you a safe and competent pilot. There's no field like it, and the industry is growing fast. Now is the time!",
         bookingLink: "https://cfidurrand.youcanbook.me/"
     },
 
-    {
-        name: "Mason Liu - Flight Instructor",
-        credentials: "CFI, CFII",
-        photo: "assets/images/Instructors/Mason Liu1.jpg",
-        specialties: ["Private", "Instrument", "Single Engine Commercial", "Flight Review", "Instrument Proficiency Check", "High Performance"],
-        approvedAircraft: ["C172", "C182FG", "PA28"], 
-        bio: "Flying has always felt like an impossible dream to me. Yet, after earning my PhD in physics and pursuing a career in finance, I decided to start flight training. I discovered where my true passion lies. My academic background and professional experience allow me to connect with both younger learners and those with rich life experience. I would be thrilled to help you achieve your goals and become a safe, capable pilot. Let's take to the skies!",
-        bookingLink: "https://masonliu.youcanbook.me/"
-    },
+    
 
     {
         name: "Oliver Emde - Flight Instructor",
